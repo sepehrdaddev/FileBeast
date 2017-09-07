e5bd27b674240da45f43f79811c003b6522d3dec
sepehrdad <sepehrdad.dev@gmail.com>
2017-09-07 22:03:43 +0400

    Initial Commit

--- modules/cryptography.py
+++ modules/cryptography.py
@@ -1,5 +1,4 @@
 import os
-from Crypto.Cipher import AES
 from Crypto import Random
 
 chunksize = 64 * 1024
@@ -8,10 +7,23 @@ chunksize = 64 * 1024
 def encrypt(key, infilepath, outfilepath, method):
     filesize = str(os.path.getsize(infilepath)).zfill(16)
     if method == 'AES':
-        IV = Random.new().read(AES.block_size)
+        from Crypto.Cipher import AES
         from Crypto.Hash import SHA256
+        IV = Random.new().read(AES.block_size)
         key = SHA256.new(key.encode('utf-8')).digest()
         encryptor = AES.new(key, AES.MODE_CBC, IV)
+    elif method == 'DES3':
+        from Crypto.Cipher import DES3
+        from Crypto.Hash import MD5
+        IV = Random.new().read(DES3.block_size)
+        key = MD5.new(key.encode('utf-8')).digest()
+        encryptor = DES3.new(key, DES3.MODE_CBC, IV)
+    elif method == 'BLOWFISH':
+        from Crypto.Cipher import Blowfish
+        from Crypto.Hash import SHA256
+        IV = Random.new().read(Blowfish.block_size)
+        key = SHA256.new(key.encode('utf-8')).digest()
+        encryptor = Blowfish.new(key, Blowfish.MODE_CBC, IV)
     with open(infilepath, 'rb') as infile:
         with open(outfilepath, 'wb') as outfile:
             outfile.write(filesize.encode('utf-8'))
@@ -30,9 +42,20 @@ def decrypt(key, infilepath, outfilepath, method):
         filesize = int(infile.read(16))
         IV = infile.read(16)
         if method == 'AES':
+            from Crypto.Cipher import AES
             from Crypto.Hash import SHA256
             key = SHA256.new(key.encode('utf-8')).digest()
             decryptor = AES.new(key, AES.MODE_CBC, IV)
+        elif method == 'DES3':
+            from Crypto.Cipher import DES3
+            from Crypto.Hash import MD5
+            key = MD5.new(key.encode('utf-8')).digest()
+            decryptor = DES3.new(key, DES3.MODE_CBC, IV)
+        elif method == 'BLOWFISH':
+            from Crypto.Cipher import Blowfish
+            from Crypto.Hash import SHA256
+            key = SHA256.new(key.encode('utf-8')).digest()
+            decryptor = Blowfish.new(key, Blowfish.MODE_CBC, IV)
         with open(outfilepath, 'wb') as outfile:
             while True:
                 chunk = infile.read(chunksize)
